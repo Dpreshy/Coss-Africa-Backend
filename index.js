8d9c488024c02d5f51e4ff355b6cdc55a7c0a1ff
Davidokih <davidyole023@gmail.com>
2023-03-21 06:16:57 +0100

    david-update

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+const express = require("express");
+const cors = require("cors");
+
+const port = 3001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Cross Africa DataBase"
+    });
+});
+
+app.listen(port, () => {
+    console.log("LIstening to server");
+});
