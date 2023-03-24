4d2f33c31d407de7ac948685cc8f00272179dbff
Davidokih <davidyole023@gmail.com>
2023-03-24 21:18:06 +0100

    update

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { RegisterUser, getAll, getSingleUser, signInUser } = require("../Controller/userController");
+const { RegisterUser, getAll, getSingleUser, signInUser, updateUser, deleteUser } = require("../Controller/UserCont/userController");
 const router = express.Router();
 
 router.route("/").get(getAll);
 router.route("/:id").get(getSingleUser);
 router.route("/register").post(RegisterUser);
 router.route("/login").post(signInUser);
+router.route("/:id/update").patch(updateUser);
+router.route("/:id/remove").delete(deleteUser);
 
 module.exports = router;
