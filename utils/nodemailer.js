c05d692da49829454fbea6274247bbad16eebc69
Davidokih <davidyole023@gmail.com>
2023-04-19 15:55:51 +0100

    update

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,14 +1,12 @@
 const nodeMailer = require('nodemailer');
 const { google } = require('googleapis');
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
 const path = require("path");
 const ejs = require("ejs");
 require("dotenv").config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REFRESH_TOKEN = "1//04tJ8CVDDrJ3KCgYIARAAGAQSNwF-L9Ir0HQZX0IbQMSrgAEkNBI9NzJWqMHHpJEUAl_08rQKXljsqLqCf1CtIqpn3Gfp_qvWUMs";
+const REFRESH_TOKEN = "1//040V4P_V5jwB-CgYIARAAGAQSNwF-L9IrhRLnXVW88WIob8oc1QKcsMOsoDlKcUuxXBLOs6TWECSueIMrSiCpipsRG-XvtoQ007w";
 const REDIRECT_URL = "https://developers.google.com/oauthplayground";
 
 const oAuthPass = new google.auth.OAuth2(
@@ -19,15 +17,9 @@ const oAuthPass = new google.auth.OAuth2(
 
 oAuthPass.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-const getToken = crypto.randomBytes(32).toString("hex");
-const token = jwt.sign({ getToken }, process.env.JWT_SECRET, { expiresIn: "3d" });
-
 const sendMail = async (user, email, otp) => {
     try {
         const createToken = await oAuthPass.getAccessToken();
-
-        // const getToken = crypto.randomBytes(32).toString("hex");
-        // const token = jwt.sign({ getToken }, "ThisIsIt", { expiresIn: "3d" });
 
         const transport = nodeMailer.createTransport({
             service: "gmail",
@@ -41,7 +33,7 @@ const sendMail = async (user, email, otp) => {
             },
         });
 
-        const buildFile = path.join(__dirname, "../views/resetPassword.ejs");
+        const buildFile = path.join(__dirname, "../views/index.ejs");
         const data = await ejs.renderFile(buildFile, {
             name: user,
             otp: otp,
