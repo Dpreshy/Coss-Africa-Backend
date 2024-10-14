23cbb3843d1794e889d091c53d287291921440d8
David Okih <98276781+Davidokih@users.noreply.github.com>
2024-10-14 13:09:33 +0100

    Update productRouter.js

--- router/productRouter.js
+++ router/productRouter.js
@@ -7,7 +7,7 @@ router.route("/").get(getAllProduct);
 router.route("/").get(searchPost);
 router.route("/:id/create/phone").post(upload.array("avatar"), createProduct);
 router.route("/:id/create/cloth").post(upload.array("avatar"), createCloth);
-router.route("/:id/create/food").post(upload.array("avatar"), createFood);
+router.route("/create/food").post(upload.array("avatar"), createFood);
 router.route("/:id/create/electronics").post(upload.array("avatar"), createElectronics);
 router.route("/:userID/:proID/remove").delete(removeProduct);
 router.route("/:productID").get(getSingleProduct);
